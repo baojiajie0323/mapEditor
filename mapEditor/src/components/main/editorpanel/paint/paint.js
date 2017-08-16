4903f6de9e0f7c973c6707b972c1acfb1577f7ce
baojiajie0323 <382058502@qq.com>
2017-08-16 13:15:25 +0800

    aaA  Please enter the commit message for your changes. Lines starting

--- mapEditor/src/components/main/editorpanel/paint/paint.js
+++ mapEditor/src/components/main/editorpanel/paint/paint.js
@@ -16,22 +16,25 @@ class Paint extends React.Component {
 
     }
     render() {
-        const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+        const text = `请在界面上选择元素`;
         return (
             <div className={styles.paint}>
                 <Collapse accordion bordered={false} defaultActiveKey={['1']}>
-                    <Panel header={<div className={[styles.title,styles.pointer].join(' ')}>选择元素</div>} key="1">
+                    <Panel header={<div className={[styles.title, styles.pointer].join(' ')}>选择元素</div>} key="1">
                         <p>{text}</p>
                     </Panel>
-                    <Panel header={<div className={[styles.title,styles.icon].join(' ')}>图标</div>} key="2">
+                    <Panel header={<div className={[styles.title, styles.icon].join(' ')}>图标</div>} key="2">
                         <p>{text}</p>
                     </Panel>
-                    <Panel header={<div className={[styles.title,styles.painter].join(' ')}>绘制元素</div>} key="3">
-                        <p>{text}</p>
+                    <Panel header={<div className={[styles.title, styles.painter].join(' ')}>绘制元素</div>} key="3">
+                        <div className={styles.itemcontent}>
+                            <p>形状</p>
+                            <div className={styles.painttype}>
+                                <div className={styles.typerect}></div>
+                                <div className={styles.typecircle}></div>
+                                <div className={styles.typepolygon}></div>
+                            </div>
+                        </div>
                     </Panel>
                 </Collapse>
             </div>
