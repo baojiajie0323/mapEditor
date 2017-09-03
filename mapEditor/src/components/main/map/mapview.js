d128117feb8b83e3c4995e4ee5690b55a138f6fe
baojiajie0323 <382058502@qq.com>
2017-09-03 20:50:14 +0800

    11

--- mapEditor/src/components/main/map/mapview.js
+++ mapEditor/src/components/main/map/mapview.js
@@ -9,6 +9,8 @@ class MapView extends React.Component {
         }
         this.mapContainer = null;
         this.mapCanvas = null;
+        this._groundHeight = 10;
+
         this.animate = this.animate.bind(this);
     }
     componentDidMount() {
@@ -22,24 +24,54 @@ class MapView extends React.Component {
     initThree() {
         console.log("initThree");
         this.renderer = new THREE.WebGLRenderer({
-            canvas: this.mapCanvas
+            canvas: this.mapCanvas,
+            antialias: true
         });
-
-        this.renderer.setClearColor(0xffffff);
+        this.renderer.setClearColor(0xf1f2f7);
         this.scene = new THREE.Scene();
-        this.camera = new THREE.OrthographicCamera(-20,20,1.5,-1.5, 1, 1000);
-        this.camera.position.set(0, 0, 5);
-        this.scene.add(this.camera);
-        var cube = new THREE.Mesh(new THREE.CubeGeometry(20, 1, 10),
-            new THREE.MeshBasicMaterial({
-                color: 0xff0000,
-                wireframe: true
-            })
-        );
-        this.scene.add(cube);
 
+        this.initCamera();
+        this.initLight();
+        this.addGround();
+        //this.addArea();
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
         this.animate();
+    }
+    initCamera() {
+        //this.camera = new THREE.OrthographicCamera(-this.mapCanvas.width / 2, this.mapCanvas.width / 2, -this.mapCanvas.height / 2, this.mapCanvas.height / 2, 1, 5000);
+        this.camera = new THREE.PerspectiveCamera(45, this.mapCanvas.width / this.mapCanvas.height, 1, 5000);
+        this.camera.position.set(500, 0, 1100);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.scene.add(this.camera);
+    }
+    initLight() {
+        var light = new THREE.DirectionalLight(0xffffff, 0.9);
+        //var light = new THREE.AmbientLight(0xffffff, 1);
+        
+        light.position.set(0, 150, 0);
+        this.scene.add(light);
+    }
+    addGround() {
+        var ground = new THREE.Mesh(new THREE.CubeGeometry(1000, this._groundHeight, 650),
+            new THREE.MeshLambertMaterial({
+                //color: 0xBBFFFF,
+                //ambient: 0x858685,
+                emissive: 0x858685,
+                //wireframe: true
+            })
+        );
+        this.scene.add(ground);
+    }
+    addArea() {
+        var area = new THREE.Mesh(new THREE.CubeGeometry(150, 30, 100),
+            new THREE.MeshLambertMaterial({
+                color: 0xd8c8df,
+                emissive: 0xd8c8df
+                //wireframe: true
+            })
+        );
+        //area.setPosition
+        this.scene.add(area);
     }
     animate() {
         requestAnimationFrame(this.animate);
