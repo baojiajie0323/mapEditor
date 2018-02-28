bd22e6deb162008e528ac4f85c0f1d3937eef3f8
baojiajie0323 <382058502@qq.com>
2018-03-01 02:15:54 +0800

    111

--- mapEditor/src/components/main/map/mapview.js
+++ mapEditor/src/components/main/map/mapview.js
@@ -10,8 +10,12 @@ class MapView extends React.Component {
         this.mapContainer = null;
         this.mapCanvas = null;
         this._groundHeight = 10;
-
-        this.animate = this.animate.bind(this);
+        this.guicontrols = {
+            rotationSpeed: 0.02,
+            bouncingSpeed: 0.03,
+            ambiColor: "#0c0c0c"
+        }
+        this.clock = new THREE.Clock();
     }
     componentDidMount() {
         var mapWidth = this.mapContainer.offsetWidth;
@@ -20,14 +24,26 @@ class MapView extends React.Component {
         this.mapCanvas.height = mapHeight;
 
         this.initStats();
+        this.initGui();
         this.initThree();
+        this.animate();
     }
     initStats() {
-        var stats = new Stats();
-        stats.setMode(0);
-        stats.domElement.style.position = 'absolute';
-        stats.domElement.style.left = '0';
-        stats.domElement.style.top = '0';
+        this.stats = new Stats();
+        this.stats.setMode(0);
+        this.stats.domElement.style.position = 'absolute';
+        this.stats.domElement.style.left = '0';
+        this.stats.domElement.style.top = '0';
+        this.statdom.append(this.stats.domElement);
+    }
+    initGui() {
+        this.gui = new dat.GUI();
+        this.gui.domElement.style.marginTop = '65px';
+        this.gui.add(this.guicontrols, 'rotationSpeed', 0, 0.5);
+        this.gui.add(this.guicontrols, 'bouncingSpeed', 0, 0.5);
+        this.gui.addColor(this.guicontrols, 'ambiColor').onChange((e) => {
+            this.ambientLight.color = new THREE.Color(e);
+        });
     }
     initThree() {
         console.log("initThree");
@@ -35,31 +51,39 @@ class MapView extends React.Component {
             canvas: this.mapCanvas,
             antialias: true
         });
-        this.renderer.setClearColor(0xf1f2f7);
+        this.renderer.setClearColor(0xeeeeee);
         this.scene = new THREE.Scene();
 
         this.initCamera();
         this.initLight();
         this.addGround();
-        //this.addArea();
-        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.addArea();
+        // this.trackballControls = new THREE.TrackballControls(this.camera);
+        // this.trackballControls.rotateSpeed = 1.0;
+        // this.trackballControls.zoomSpeed = 1.0;
+        // this.trackballControls.panSpeed = 1.0;
+
+        this.orbitControls = new THREE.OrbitControls(this.camera);
     }
     initCamera() {
         //this.camera = new THREE.OrthographicCamera(-this.mapCanvas.width / 2, this.mapCanvas.width / 2, -this.mapCanvas.height / 2, this.mapCanvas.height / 2, 1, 5000);
         this.camera = new THREE.PerspectiveCamera(45, this.mapCanvas.width / this.mapCanvas.height, 1, 5000);
-        this.camera.position.set(500, 0, 1100);
+        this.camera.position.set(0, 0, 2000);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
         this.scene.add(this.camera);
     }
     initLight() {
+        this.ambientLight = new THREE.AmbientLight(this.guicontrols.ambiColor);
+        this.scene.add(this.ambientLight);
+
         var light = new THREE.DirectionalLight(0xffffff, 0.8);
         //var light = new THREE.AmbientLight(0xffffff, 1);
-        
-        light.position.set(10, 150, 10);
+
+        light.position.set(0, 0, 1000);
         this.scene.add(light);
     }
     addGround() {
-        var ground = new THREE.Mesh(new THREE.CubeGeometry(1000, this._groundHeight, 650),
+        var ground = new THREE.Mesh(new THREE.CubeGeometry(1540, 858, this._groundHeight),
             new THREE.MeshLambertMaterial({
                 color: 0xf1f6f7,
                 ambient: 0x858685,
@@ -72,15 +96,16 @@ class MapView extends React.Component {
     addArea() {
         var area = new THREE.Mesh(new THREE.CubeGeometry(150, 30, 100),
             new THREE.MeshLambertMaterial({
-                color: 0xd8c8df,
-                emissive: 0xd8c8df
-                //wireframe: true
-            })
-        );
+                color: 0x7777ff,
+                wrapAround: true
+            }))
         //area.setPosition
         this.scene.add(area);
     }
     animate = () => {
+        this.stats.update();
+        var delta = this.clock.getDelta;
+        this.orbitControls.update(delta);
         requestAnimationFrame(this.animate);
         this.renderer.render(this.scene, this.camera);
         //stats.update();
@@ -92,7 +117,7 @@ class MapView extends React.Component {
     render() {
         return <div ref={(c) => { this.mapContainer = c }} className={styles.mapview}>
             <canvas ref={(c) => { this.mapCanvas = c }} id="mapView" ></canvas>
-            <div id="Stat-output"></div>
+            <div ref={(c) => { this.statdom = c }}></div>
         </div>
     }
 }
